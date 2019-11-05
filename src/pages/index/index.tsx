7ec7e8a15a71bd1f28b6392b4024012ad6b8a667
何喜阳 <iamxiyang@foxmail.com>
2019-11-05 09:27:21 +0800

    updata ： 优化结构

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -9,20 +9,20 @@ import {ClassifyModel} from "../../models/ClassifyModel";
 import {ClassifyData, GoodsData, SwiperData} from "../../data";
 import {GoodsModel} from "../../models/GoodsModel";
 import {SeckillGoodsModel} from "../../models/SeckillGoodsModel";
+
 export interface Props {
 
 }
 
 export interface State {
-  value: string,
-  location: LocationModel
-  swipers: SwiperModel[]
-  classify: ClassifyModel[]
-  adBanner: string,
-  goodsList:SeckillGoodsModel[]
-  imgData:any,
-  current:number
-  RecommendList:GoodsModel[],
+  value: string,//搜索词
+  location: LocationModel,//定位
+  swipers: SwiperModel[],//轮播
+  classify: ClassifyModel[],//首页分类
+  adBanner: string,//首页宣传广告
+  goodsList: SeckillGoodsModel[],//首页秒杀商品
+  current: number,//秒杀选项卡
+  recommendList: GoodsModel[],//首页推荐商品
 }
 
 export default class Index extends Component<Props, State> {
@@ -48,13 +48,9 @@ export default class Index extends Component<Props, State> {
       swipers: [],
       classify: [],
       adBanner: '../../statics/imgs/banner.png',
-      imgData:[
-        '../../statics/imgs/banner1.png',
-        '../../statics/imgs/banner2.png',
-      ],
-      current:0,
-      RecommendList:GoodsData,
-      goodsList:GoodsData
+      current: 0,
+      recommendList: GoodsData,
+      goodsList: GoodsData
     }
   }
 
@@ -87,11 +83,13 @@ export default class Index extends Component<Props, State> {
       url: `/pages/classify/index?id=${index || item}`
     })
   };
-  handleClick (value) {
+
+  handleClick(value) {
     this.setState({
       current: value
     })
   };
+
   render() {
     return (
       <View className='index'>
@@ -127,176 +125,107 @@ export default class Index extends Component<Props, State> {
                 data={this.state.classify}/>
 
         <View className="banner">
-          <Image src={this.state.adBanner} className="banner__img"></Image>
+          <Image src={this.state.adBanner} className="banner__img"/>
         </View>
 
         <View className='at-row container recommend-box'>
-          {this.state.imgData.map((it, index) => {
-            return    <Navigator url={`/pages/goods/good?type=${index}`} className='at-col' key={'img'+index}><Image src={it} className={"recommend_Img"}></Image></Navigator>
-          })}
+          <Navigator url={`/pages/goods/good`} className='at-col'>
+            <Image src={'../../statics/imgs/banner1.png'}
+                   className={"recommend_Img"}/>
+          </Navigator>
+          <Navigator url={`/pages/goods/new`} className='at-col'>
+            <Image src={'../../statics/imgs/banner2.png'}
+                   className={"recommend_Img"}/>
+          </Navigator>
         </View>
 
 
-        <View className={'bg-color--white'} style={{padding:'44px 0 0',marginBottom:'11px'}}>
+        <View className={'bg-color--white'} style={{padding: '32px 0 0', marginBottom: '11px'}}>
           <View className={'count-down bg-color--F12737 kill-Time'}>
-            <View className={'inline--block margin-right--10'}><IconFont name='huo'size={32} color='#fff'></IconFont></View>
+            <View className={'inline--block margin-right--10'}>
+              <IconFont name='huo' size={32} color='#fff'/>
+            </View>
             <Text className={'c--fff f__size--30 margin-right--10'}>秒杀抢购</Text>
             <Text className={'c--fff f__size--22'}>先到先得，赶紧抢货</Text>
             <View className={'fr'}>
               <Text className={'c--fff f__size--22 margin-right--10'}>距离结束</Text>
               <AtCountdown
-                format={{ hours: ':', minutes: ':', seconds: '' }}
+                format={{hours: ':', minutes: ':', seconds: ''}}
                 isCard
                 minutes={1}
                 seconds={10}
               />
             </View>
           </View>
+
           <AtTabs
             current={this.state.current}
             scroll
             tabList={[
-              { title: '标签页1', },
-              { title: '标签页2' },
-              { title: '标签页3' },
-              { title: '标签页4' },
-              { title: '标签页5' },
-              { title: '标签页6' }
+              {title: '标签页1',},
+              {title: '标签页2'},
+              {title: '标签页3'},
+              {title: '标签页4'},
+              {title: '标签页5'},
+              {title: '标签页6'}
             ]}
             onClick={this.handleClick.bind(this)}>
-            <AtTabsPane current={this.state.current} index={0}>
-              <View style='font-size:18px;text-align:center;'>
-                <View className={'text--left'}>
-                  {this.state.goodsList.map((it, index) => {
-                    return  <View  key={'img'+index} style={{border:'1px solid #F5F5F5',marginTop:'20px'}}>
-                      <Image src={it.product_img} className={"recommend_Img"}></Image>
-                      <View style={{padding:'0px 10px'}}>
-                        <View className={'f__size--28 c--010 margin-top--20'}>{it.product_name}
-                          <View className={'margin-top--20'}>
-                            <Text className={'c--eb3 f__size--30 margin-right--20'}>￥{it.product_original}</Text><Text className={'c--808 f__size--24'}>￥{it.product_price}</Text>
+
+            {[0, 1, 2, 3, 4, 5].map(num => {
+              return <AtTabsPane current={this.state.current} index={num} key={'tab' + num}>
+                <View style={{fontSize: '18px', textAlign: 'center'}}>
+                  <View className={"text--left"}>
+                    {this.state.goodsList.map((it, index) => {
+                      return <View key={'img' + index} style={{border: '1px solid #F5F5F5', marginTop: '20px'}}>
+                        <Image src={it.product_img} className={"recommend_Img"}/>
+                        <View style={{padding: '0px 10px'}}>
+                          <View className={'f__size--28 c--010 margin-top--20'}>{it.product_name}
+                            <View className={'margin-top--20'}>
+                              <Text className={'c--eb3 f__size--30 margin-right--20'}>￥{it.product_original}</Text><Text
+                              className={'c--808 f__size--24'}>￥{it.product_price}</Text>
+                            </View>
+                          </View>
+                          <View className={'flex'} style={{padding: '10px 0'}}>
+                            <Progress percent={it.product_jindu} style={{width: '30%'}} borderRadius={20}
+                                      activeColor={'#F12737'}/>
+                            <Text className={'f__size--22 c--808 margin-left--20'}>已抢{it.product_jindu}%</Text>
+                            <Navigator url={`/pages/goods/detail?id=${it.product_id}`} style={'margin-left:auto'}>
+                              <AtButton type='primary' size={'small'} circle={true}
+                                        className={'rush--buy'}>马上抢购</AtButton>
+                            </Navigator>
                           </View>
                         </View>
-                        <View className={'flex'} style={{padding:'10px 0'}}>
-                          <Progress percent={it.product_jindu} style={{width:'30%'}} borderRadius={20} activeColor={'#F12737'}/>
-                          <Text className={'f__size--22 c--808 margin-left--20'}>已抢{it.product_jindu}%</Text>
-                          <View style={'margin-left:auto'}>
-                            <AtButton type='primary' size={'small'} circle={true} className={'rush--buy'}>马上抢购</AtButton>
-                          </View>
-                        </View>
+
                       </View>
 
-                    </View>
-
-                  })}
+                    })}
+                  </View>
                 </View>
-              </View>
-            </AtTabsPane>
-            <AtTabsPane current={this.state.current} index={1}>
-              <View style='font-size:18px;text-align:center;'>已开抢</View>
-            </AtTabsPane>
-            <AtTabsPane current={this.state.current} index={2}>
-              <View style='font-size:18px;text-align:center;'>已开抢</View>
-            </AtTabsPane>
-            <AtTabsPane current={this.state.current} index={3}>
-              <View style='font-size:18px;text-align:center;'>
-                <View className={'text--left'}>
-                  {this.state.goodsList.map((it, index) => {
-                    return  <View  key={'img'+index} style={{border:'1px solid #F5F5F5',marginTop:'20px'}}>
-                      <Image src={it.product_img} className={"recommend_Img"}></Image>
-                      <View style={{padding:'0px 10px'}}>
-                        <View className={'f__size--28 c--010 margin-top--20'}>{it.product_name}
-                          <View className={'margin-top--20'}>
-                            <Text className={'c--eb3 f__size--30 margin-right--20'}>￥{it.product_original}</Text><Text className={'c--808 f__size--24'}>￥{it.product_price}</Text>
-                          </View>
-                        </View>
-                        <View className={'flex'} style={{padding:'10px 0'}}>
-                          <Progress percent={it.product_jindu} style={{width:'30%'}} borderRadius={20} activeColor={'#F12737'}/>
-                          <Text className={'f__size--22 c--808 margin-left--20'}>已抢{it.product_jindu}%</Text>
-                          <View style={'margin-left:auto'}>
-                            <AtButton type='primary' size={'small'} circle={true} className={'rush--buy'}>马上抢购</AtButton>
-                          </View>
-                        </View>
-                      </View>
+              </AtTabsPane>
+            })}
 
-                    </View>
-
-                  })}
-                </View>
-              </View>
-            </AtTabsPane>
-            <AtTabsPane current={this.state.current} index={4}>
-              <View style='font-size:18px;text-align:center;'>
-                <View className={'text--left'}>
-                  {this.state.goodsList.map((it, index) => {
-                    return  <View  key={'img'+index} style={{border:'1px solid #F5F5F5',marginTop:'20px'}}>
-                      <Image src={it.product_img} className={"recommend_Img"}></Image>
-                      <View style={{padding:'0px 10px'}}>
-                        <View className={'f__size--28 c--010 margin-top--20'}>{it.product_name}
-                          <View className={'margin-top--20'}>
-                            <Text className={'c--eb3 f__size--30 margin-right--20'}>￥{it.product_original}</Text><Text className={'c--808 f__size--24'}>￥{it.product_price}</Text>
-                          </View>
-                        </View>
-                        <View className={'flex'} style={{padding:'10px 0'}}>
-                          <Progress percent={it.product_jindu} style={{width:'30%'}} borderRadius={20} activeColor={'#F12737'}/>
-                          <Text className={'f__size--22 c--808 margin-left--20'}>已抢{it.product_jindu}%</Text>
-                          <View style={'margin-left:auto'}>
-                            <AtButton type='primary' size={'small'} circle={true} className={'rush--buy'}>马上抢购</AtButton>
-                          </View>
-                        </View>
-                      </View>
-                    </View>
-                  })}
-                </View>
-              </View>
-            </AtTabsPane>
-            <AtTabsPane current={this.state.current} index={5}>
-              <View style='font-size:18px;text-align:center;'>
-                <View className={'container'}>
-                  {this.state.goodsList.map((it, index) => {
-                    return  <View  key={'img'+index} style={{border:'1px solid #F5F5F5',marginTop:'20px'}}>
-                      <Image src={it.product_img} className={"recommend_Img"}></Image>
-                      <View style={{padding:'0px 10px'}}>
-                        <View className={'f__size--28 c--010 margin-top--20'}>{it.product_name}
-                          <View className={'margin-top--20'}>
-                            <Text className={'c--eb3 f__size--30 margin-right--20'}>￥{it.product_original}</Text><Text className={'c--808 f__size--24'}>￥{it.product_price}</Text>
-                          </View>
-                        </View>
-                        <View className={'flex'} style={{padding:'10px 0'}}>
-                          <Progress percent={it.product_jindu} style={{width:'30%'}} borderRadius={20} activeColor={'#F12737'}/>
-                          <Text className={'f__size--22 c--808 margin-left--20'}>已抢{it.product_jindu}%</Text>
-                          <View style={'margin-left:auto'}>
-                            <AtButton type='primary' size={'small'} circle={true} className={'rush--buy'}>马上抢购</AtButton>
-                          </View>
-                        </View>
-                      </View>
-
-                    </View>
-
-                  })}
-                </View>
-              </View>
-            </AtTabsPane>
           </AtTabs>
         </View>
         <View className={'container margin-bottom--20'}>
-          <AtDivider content='为你推荐' fontColor='#F12737' lineColor='#F12737' />
+          <AtDivider content='为你推荐' fontColor='#F12737' lineColor='#F12737'/>
         </View>
         <View className='at-row at-row--wrap RecommendList container bg-color--f2f2f2'>
-            {this.state.RecommendList.map((it, index) => {
-      return  <View className='at-col at-col-6'>
-              <View  key={'img'+index} style={{background:'#fff'}}>
-                <Image src={it.product_img} className={"RecommendList--img"}></Image>
+          {this.state.recommendList.map((it, index) => {
+            return <Navigator url={`/pages/goods/detail?id=${it.product_id}`} className='at-col at-col-6'>
+              <View key={'img' + index} style={{background: '#fff'}}>
+                <Image src={it.product_img} className={"RecommendList--img"}/>
                 <View style={'padding:5px'}>
                   <View className={'margin-top--10 '} style={'height:40px'}>
                     <Text className={'c--333 f__size--24'}>{it.product_name}</Text>
                   </View>
-                  <View >
-                    <Text className={'c--eb3 f__size--30 margin-right--20'}>￥{it.product_price}</Text><Text className={'f__size--24 c--808 text--line-through'}>￥{it.product_original}</Text>
+                  <View>
+                    <Text className={'c--eb3 f__size--30 margin-right--20'}>￥{it.product_price}</Text><Text
+                    className={'f__size--24 c--808 text--line-through'}>￥{it.product_original}</Text>
                   </View>
                 </View>
               </View>
-            </View>
-            })}
+            </Navigator>
+          })}
         </View>
       </View>
     )
