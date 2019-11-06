8040013a051f9067d9b4d18da44631e7851ff09f
何喜阳 <iamxiyang@foxmail.com>
2019-11-06 15:39:25 +0800

    feat：购物车实现

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,10 +1,12 @@
 import Taro, {Component, Config} from '@tarojs/taro'
-import {View, Text, Image, Button, Label} from '@tarojs/components'
+import {View, Text, Image, Button, Label, Navigator, Block} from '@tarojs/components'
 import './index.scss'
 import {GoodsData} from "../../data";
 import {GoodsModel} from "../../models/GoodsModel";
 import IconFont from "../../components/iconfont";
-import {AtInputNumber} from "taro-ui";
+import {AtDivider, AtInputNumber} from "taro-ui";
+import GoodsList from "../../components/goods-list/goods-list";
+import StateTip from "../../components/state-tip/state-tip";
 
 export interface Props {
 
@@ -12,7 +14,12 @@ export interface Props {
 
 export interface State {
   isEdit: boolean,//是否在编辑状态
-  cartList: GoodsModel[]
+  cartList: GoodsModel[],
+  goodsList: GoodsModel[],
+  checkedNumber: number,//已经选中的商品数量
+  isAllChecked: boolean,//是否全选
+  totalNumber: number,//商品总数量
+  totalPrice: number,//商品总金额
 }
 
 export default class Index extends Component<Props, State> {
@@ -24,7 +31,9 @@ export default class Index extends Component<Props, State> {
    * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
    * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
    */
-  config: Config = {};
+  config: Config = {
+    navigationBarTitleText: "购物车"
+  };
 
   static defaultProps = {};
 
@@ -32,7 +41,13 @@ export default class Index extends Component<Props, State> {
     super(props);
     this.state = {
       isEdit: false,
-      cartList: GoodsData
+      // cartList: [],
+      cartList: GoodsData,
+      goodsList: GoodsData,
+      isAllChecked: false,
+      totalNumber: 0,
+      totalPrice: 0,
+      checkedNumber: 0
     }
   }
 
@@ -54,19 +69,45 @@ export default class Index extends Component<Props, State> {
   // 选择、不选某一项
   onCheck = (index) => {
     console.log('目前选中的项目:', index);
-    const {cartList} = this.state;
+    const {cartList,} = this.state;
+    let {isAllChecked, checkedNumber} = this.state;
     cartList[index]['checked'] = !cartList[index]['checked'];
-    this.setState({cartList});
+    if (cartList[index]['checked']) {
+      checkedNumber++
+    } else {
+      checkedNumber--
+    }
+    isAllChecked = checkedNumber === cartList.length;
+
+    this.setState({cartList, isAllChecked, checkedNumber}, () => {
+      this.computeTotalPrice()
+    });
   };
 
   // 全选、全不选
   onCheckAll = () => {
-
+    let {cartList, isAllChecked} = this.state;
+    isAllChecked = !isAllChecked;
+    cartList.forEach(it => {
+      it['checked'] = isAllChecked;
+    });
+    this.setState({isAllChecked, cartList, checkedNumber: cartList.length}, () => {
+      this.computeTotalPrice()
+    })
   };
 
-  // 计算费用
+  // 计算费用和数量
   computeTotalPrice = () => {
-
+    const {cartList} = this.state;
+    let totalPrice = 0;
+    let totalNumber = 0;
+    cartList.forEach(it => {
+      if (it['checked']) {
+        totalNumber += it['num'] * 1 || 1;
+        totalPrice += (it['num'] * 1 || 1) * it.product_price;
+      }
+    });
+    this.setState({totalPrice, totalNumber})
   };
 
   // 进入、退出编辑
@@ -79,21 +120,81 @@ export default class Index extends Component<Props, State> {
     console.log(index, num);
     const {cartList} = this.state;
     cartList[index]['num'] = num;
-    this.setState({cartList});
+    this.setState({cartList}, () => {
+      this.computeTotalPrice()
+    });
   };
 
   // 结算
   onSubmit = () => {
+    const {totalNumber, totalPrice} = this.state;
+    if (!totalNumber) {
+      Taro.showToast({
+        icon: 'none',
+        title: `请先选择商品`
+      });
+      return;
+    }
+    Taro.showToast({
+      icon: 'none',
+      title: `总费用是${totalPrice}`
+    });
+    this.setState({cartList: []})
+  };
+
+  // 删除已选中的
+  onDelChecked = (index) => {
+    Taro.showModal({
+      title: '询问',
+      content: '真的要删除商品吗？'
+    }).then(res => {
+      if (res.confirm) {
+        const {cartList} = this.state;
+        let {checkedNumber} = this.state;
+        if (index >= 0) {
+          const it = cartList[index];
+          if (it['checked']) {
+            checkedNumber--;
+          }
+          cartList.splice(index, 1);
+        } else {
+          cartList.forEach((it, index) => {
+            if (it['checked']) {
+              checkedNumber--;
+              cartList.splice(index, 1);
+            }
+          });
+        }
+        this.setState({cartList, checkedNumber}, () => {
+          this.computeTotalPrice()
+        })
+      }
+    });
 
   };
 
   render() {
+
+    if (!this.state.cartList || this.state.cartList.length < 1) {
+      return (
+        <View>
+          <StateTip size={"small"} imgUrl={'../../statics/imgs/tabbar/shop_no.png'} title={'购物车空空如也'}>
+            <Navigator url={"/pages/goods/good"} className={"btn-go-shop"}>逛逛今日特卖</Navigator>
+          </StateTip>
+          <View style={{width: '200px', margin: '0 auto '}}>
+            <AtDivider content='猜你喜欢' fontSize={30} fontColor='#EB3939' lineColor='#EB3939'/>
+          </View>
+          <GoodsList data={this.state.goodsList}/>
+        </View>
+      )
+    }
+
     return (
       <View className='index'>
         <View className="container bg-color--f2f2f2 flex a__items--center j__content--spbe">
           <Text className="f__size--28">
             共
-            <Text className="c--eb3">{this.state.cartList.length || 0}</Text>
+            <Text className="c--eb3 margin-left--10 margin-right--10">{this.state.totalNumber || 0}</Text>
             件商品
           </Text>
           <Button className={"btn btn-edit"} onClick={this.onEdit}>
@@ -107,33 +208,64 @@ export default class Index extends Component<Props, State> {
                      onClick={() => {
                        this.onCheck(index)
                      }}>
-                <IconFont name={"duihao"} size={28}/>
+                {it['checked'] && <IconFont name={"duihao"} size={28}/>}
               </Label>
               <Image src={it.product_img} className={"goods__cover"}/>
               <View className="cart__content">
-                <Text className="block name">{it.product_name}</Text>
-                <Text className="block name">{it.product_original}</Text>
-                <Text className="block">{it.product_original}</Text>
-                <AtInputNumber
-                  className={"input-number"}
-                  min={1}
-                  // max={10}
-                  step={1}
-                  value={it['num'] || 1}
-                  type={"number"}
-                  onChange={(event)=>{
-                    this.onChangeNum(index, event)
-                  }}
-                />
+                <Text className="block name ellipsis-2">{it.product_name}</Text>
+                <Text className="block sku ellipsis-2">{it.product_original}</Text>
+                <Text className="block price">￥{it.product_price}</Text>
+
+                {this.state.isEdit ?
+                  <Block>
+                    <AtInputNumber
+                      className={"input-number"}
+                      min={1}
+                      // max={10}
+                      step={1}
+                      value={it['num'] || 1}
+                      type={"number"}
+                      onChange={(event) => {
+                        this.onChangeNum(index, event)
+                      }}
+                    />
+                    <Button className={"btn-del"} onClick={() => {
+                      this.onDelChecked(index)
+                    }}>
+                      <IconFont name={"shanchu"} size={30}/>
+                    </Button>
+                  </Block> : <Text className="input-number">x {it['num'] || 1}</Text>}
               </View>
             </View>
           })}
         </View>
 
-        <View className="footer">
+        <View className="container footer">
+          <Label className="flex__1 flex a__items--center" onClick={this.onCheckAll}>
+            {this.state.isAllChecked ?
+              <View className={`checkbox inline checked`}>
+                <IconFont name={"duihao"} size={28}/>
+              </View>
+              : <View className={`checkbox inline `}>
+              </View>
+            }
+            <Text className="c--666">全选</Text>
+          </Label>
+          <View>
+
+            {!this.state.isEdit &&
+            <Block>
+                <Text className="c--333">总计</Text>
+                <Text className="c--eb3">￥{this.state.totalPrice}</Text>
+            </Block>}
+
+          </View>
+
+          {this.state.isEdit ?
+            <Button className="btn" onClick={this.onDelChecked}>删除</Button> :
+            <Button className="btn" onClick={this.onSubmit}>结算</Button>}
 
         </View>
-
       </View>
     )
   }
