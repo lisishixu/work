7718b821afc77b4ab7dfcaa5f5c6c8ddbc770882
slice_jing <jingjing2lele@qq.com>
2019-11-13 09:53:42 +0800

    页面

--- src/pages/setting/userSet.tsx
+++ src/pages/setting/userSet.tsx
@@ -46,10 +46,18 @@ export default class UserSet extends Component<Props, State> {
             <Image src={'../../statics/imgs/banner1.png'}/>
           </View>
           <AtList>
-            <AtListItem title='标题文字' />
-            <AtListItem title='标题文字' arrow='right' />
-            <AtListItem title='标题文字' extraText='详细信息'  arrow='right'/>
-            <AtListItem title='禁用状态' disabled extraText='详细信息' />
+            <AtListItem title='昵称' extraText='包大人' arrow='right'/>
+            <AtListItem title='会员ID' extraText='11122222222' arrow='right'/>
+            <AtListItem title='性别' extraText='女' arrow='right'/>
+            <AtListItem title='生日' extraText='未填写' arrow='right'/>
+            <AtListItem title='电话号码' extraText='去绑定' arrow='right'/>
+          </AtList>
+          <AtList>
+            <AtListItem title='所在地区' extraText='金水区' arrow='right'/>
+            <AtListItem title='我的邀请人' extraText='kris吴'/>
+            <AtListItem title='性别' extraText='女'/>
+            <AtListItem title='生日' extraText='未填写' />
+            <AtListItem title='电话号码' extraText='去绑定'/>
           </AtList>
         </View>
       </View>
