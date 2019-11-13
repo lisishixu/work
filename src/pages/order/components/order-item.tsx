3fbffd1afa9744658ef974ae408de907835b0c15
何喜阳 <iamxiyang@foxmail.com>
2019-11-13 09:32:59 +0800

    订单列表

---
+++ src/pages/order/components/order-item.tsx
@@ -0,0 +1,111 @@
+import Taro, {Component, Config} from '@tarojs/taro'
+import {View, Text, Navigator, Image, Button} from '@tarojs/components'
+import './order-item.scss'
+
+export interface Props {
+
+}
+
+export interface State {
+
+}
+
+export default class OrderItem extends Component<Props, State> {
+
+  config: Config = {};
+
+  static defaultProps = {};
+
+  static options = {
+    addGlobalClass: true
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  componentWillMount() {
+  }
+
+  componentDidMount() {
+  }
+
+  componentWillUnmount() {
+  }
+
+  componentDidShow() {
+  }
+
+  componentDidHide() {
+  }
+
+  render() {
+    // let btns = ``;
+    // let state = ``;
+    // switch (0) {
+    //   case '0':
+    //   case 0:
+    //     state = `待付款`;
+    //     btns = ``;
+    //     break;
+    //   case '1' :
+    //   case  1:
+    //     state = `待发货`;
+    //     btns = ` `;
+    //     break;
+    //   case '2' :
+    //   case  2:
+    //     state = `待收货`;
+    //     btns = ``;
+    //     break;
+    //   case '3' :
+    //   case 3:
+    //     state = `待评价`;
+    //     btns = ``;
+    //     break;
+    //   case '4':
+    //   case 4:
+    //     state = `已完成`;
+    //     btns = ``;
+    //     break;
+    // }
+
+    return (
+      <View className='order-item'>
+
+
+        <View className="header flex a__items--center j__content--spbe">
+          <View>
+            <Text className="mark ">买</Text>
+            <Text className="c--666 f__size--28 order__id">订单号：201545748782745875457</Text>
+          </View>
+          <Text className="c--eb3 f__size--28">待付款</Text>
+        </View>
+        <Navigator className="goods flex a__items--center">
+          <Image src="http://yanxuan.nosdn.127.net/425c5a909f5806d55c151457a5baa0af.png" className="goods__cover"/>
+          <View className="content">
+            <View className="flex a__items--center j__content--spbe">
+              <Text className="c--333 f__size--30 ellipsis-2">SunRype 莓水果条 14克</Text>
+              <Text className="c--333">￥26.8</Text>
+            </View>
+            <View className="flex a__items--center j__content--spbe margin-top--20">
+              <Text className="c--666 f__size--26">规格：14g</Text>
+              <Text className="c--666 f__size--26">x 1</Text>
+            </View>
+          </View>
+        </Navigator>
+        <View className="order__total c--666 f__size--28 margin-left--auto">
+          <Text className="margin-right--20">共2件商品</Text>
+          <Text className="">
+            实付：<Text className="c--010 f__size--30">￥39.4</Text>
+          </Text>
+        </View>
+        <View className="order__btn-wrap ">
+          <Button className="btn btn-secondary">取消</Button>
+          <Button className="btn btn-highlight">去付款</Button>
+        </View>
+      </View>
+    )
+  }
+}
--- src/pages/order/index.tsx
+++ src/pages/order/index.tsx
@@ -1,13 +1,15 @@
 import Taro, {Component, Config} from '@tarojs/taro'
-import {View, Text} from '@tarojs/components'
+import {View} from '@tarojs/components'
 import './index.scss'
+import {AtTabs, AtTabsPane} from "taro-ui";
+import OrderItem from "./components/order-item";
 
 export interface Props {
 
 }
 
 export interface State {
-
+  currentTab: number
 }
 
 export default class OrderIndex extends Component<Props, State> {
@@ -20,7 +22,9 @@ export default class OrderIndex extends Component<Props, State> {
 
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      currentTab: 0
+    }
   }
 
   componentWillMount() {
@@ -38,11 +42,49 @@ export default class OrderIndex extends Component<Props, State> {
   componentDidHide() {
   }
 
+  handleClick = (index) => {
+    if (index === 4) {
+      Taro.navigateTo({
+        url: '/pages/estimate/estimate'
+      });
+      return;
+    }
+    this.setState({currentTab: index})
+  };
+
+
   render() {
+
+
     return (
       <View className='index'>
 
 
+        <AtTabs
+          customStyle={{borderTop:'4px solid #f2f2f2'}}
+          current={this.state.currentTab}
+          tabList={[
+            {title: '全部',},
+            {title: '待付款'},
+            {title: '待发货'},
+            {title: '待收货'},
+            {title: '待评价'},
+          ]}
+          onClick={this.handleClick.bind(this)}>
+
+          {[0, 1, 2, 3, 4, 5].map((num) => {
+            return <AtTabsPane key={'goodslist' + num}
+                               current={this.state.currentTab}
+                               index={num}>
+
+              {[0, 1, 2, 3].map(() => {
+                return <OrderItem/>
+              })}
+
+            </AtTabsPane>
+          })}
+
+        </AtTabs>
       </View>
     )
   }
