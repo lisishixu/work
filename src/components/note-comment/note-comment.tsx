7129ea03370e20560f1e8b873c3fc95a49ce69f4
何喜阳 <iamxiyang@foxmail.com>
2019-11-14 09:57:29 +0800

    update

--- src/components/note-comment/note-comment.tsx
+++ src/components/note-comment/note-comment.tsx
@@ -81,13 +81,28 @@ export default class NoteComment extends Component<Props, State> {
           <View className="content" onClick={() => this.setState({isOpened: true})}>
             聚划算杀机发送卡和科三聚划算杀机发送卡和科三聚划算杀机发送卡和科三聚划算杀机发送卡和科三聚划算杀机发送卡和科三聚划算杀机
           </View>
-        {/*  todo 二级回复样式暂未制作 */}
+
+          {1 + 1 > 1 &&
+          <View className='note-comment note-comment--secondary'>
+              <View className="header">
+                  <Navigator url={"/pages/user/homepage?userID="} className={"header__left"}>
+                      <AtAvatar circle  customStyle={{width: '30px', height: '30px'}}/>
+                      <Text className="username">是又优雅</Text>
+                      <Text className="post-time">12:37</Text>
+                  </Navigator>
+              </View>
+              <View className="content" onClick={() => this.setState({isOpened: true})}>
+                  聚划算杀机发送卡和科三聚划算杀机发送卡和科三聚划算杀机发送卡和科三聚划算杀机发送卡和科三聚划算杀机发送卡和科三聚划算杀机
+              </View>
+          </View>}
+
         </View>
+
         <AtActionSheet isOpened={this.state.isOpened} cancelText='取消' onClose={() => this.setState({isOpened: false})}>
           <AtActionSheetItem onClick={this.onReply}>
             回复
           </AtActionSheetItem>
-          <AtActionSheetItem  onClick={this.onReport}>
+          <AtActionSheetItem onClick={this.onReport}>
             举报
           </AtActionSheetItem>
         </AtActionSheet>
