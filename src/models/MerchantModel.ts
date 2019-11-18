b999fa852733b97daf5b06dad43347d29e041873
何喜阳 <iamxiyang@foxmail.com>
2019-11-18 13:06:55 +0800

    update

---
+++ src/models/MerchantModel.ts
@@ -0,0 +1,10 @@
+export interface MerchantModel {
+  id?: string,//店铺编号
+  address: string,//店铺地址
+  name: string,//店主姓名
+  cover?: string,//门头图
+  phone: string,
+  idcard: string,//身份证
+  businessLicense?: string,//营业执照
+  time: string | number,//入驻时间
+}
